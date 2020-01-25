d2a19d871074247c7e40ca008a7e9cd52a75593d
LachlanMares <30546646+LachlanMares@users.noreply.github.com>
2020-01-25 10:52:34 +1000

    Add files via upload

---
+++ GetData.py
@@ -0,0 +1,37 @@
+import yahoo_finance_pynterface as yahoo
+import pandas as pd
+
+if __name__ == '__main__':
+
+    path = 'C:/Users/lachl/PycharmProjects/ShareData/'
+
+    indicies = ["^GSPC", "QQQ", "^IXIC"]
+
+    stockNames = ["BLDP", "AAPL", "MSFT", "FCEL", "NVAX", "QCOM", "CSCO", "PLUG", "INTC", "NFLX",
+                  "GILT", "GE", "MGIC", "SBUX", "ADBE", "AMZN", "LBTYK", "SINA", "LPSN", "JBLU", "AMAT", "GOOG",
+                  "BEAT", "DLTR", "LSCC", "LRCX", "SIRI", "NURO", "TSCO", "LOGI", "OPNT", "CGNX", "GILD", "IIVI",
+                  "ADSK", "COST", "CME", "ISRG", "NVDA", "SPNS", "TSEM"]
+
+    period = ['2000-01-01','2020-01-20']
+
+    for indicie in indicies:
+        print(indicie)
+        prices = pd.DataFrame(yahoo.Get.Prices(indicie, period=period), columns=['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']).reset_index('Date (UTC)')
+        prices['Date (UTC)'] = prices['Date (UTC)'].dt.date
+        prices.to_csv(path + "Indicies/" + indicie + ".csv", index=False)
+
+    for stock in stockNames:
+        print(stock)
+
+        prices = pd.DataFrame(yahoo.Get.Prices(stock, period=period), columns=['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']).reset_index('Date (UTC)')
+        prices['Date (UTC)'] = prices['Date (UTC)'].dt.date
+
+        try:
+            dividend = pd.DataFrame(yahoo.Get.Dividends(stock, period=period), columns=['Dividends']).reset_index('Date (UTC)')
+            dividend['Date (UTC)'] = dividend['Date (UTC)'].dt.date
+            prices = prices.merge(dividend, how='outer', left_on='Date (UTC)', right_on='Date (UTC)')
+
+        except:
+           prices['Dividends'] = 0.0
+
+        prices.to_csv(path + "Shares/" + stock +".csv", index=False)
